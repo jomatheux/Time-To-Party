d790a458f61f75aba6f849f922f7870604571092
jomatheux <jomatheux@gmail.com>
2024-11-26 17:44:43 -0300

    Resgatando um serviço no banco de dados

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,9 +1,13 @@
-import {Service as ServiceModel} from '../models/Service.js';
+import { response } from 'express';
+import { Service as ServiceModel } from '../models/Service.js';
+import mongoose from 'mongoose';
 
 const serviceController = {
-    create: async(req, res) => {
+
+    // Função para criar service
+    create: async (req, res) => {
         try {
-            
+
             const service = {
                 name: req.body.name,
                 description: req.body.description,
@@ -13,12 +17,47 @@ const serviceController = {
 
             const response = await ServiceModel.create(service);
 
-            res.status(201).json({response, msg: 'Serviço criado com sucesso'});
+            res.status(201).json({ response, msg: 'Serviço criado com sucesso' });
 
         } catch (error) {
             console.log(error);
         }
+    },
+
+    // Função para listar todos os services
+    getAll: async (req, res) => {
+        try {
+            const response = await ServiceModel.find();
+
+            res.status(200).json({ response, msg: 'Todos os serviços recuperados com sucesso' });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    // Função para buscar um service pelo id
+    get: async (req, res) => {
+        try {
+
+            const { id } = req.params;
+            
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                return res.status(400).json({ msg: 'ID inválido' });
+            }
+
+            const response = await ServiceModel.findById(req.params.id);
+
+            if (!response) {
+                res.status(404).json({ msg: "Serviço não encontrado." });
+                return;
+            }
+
+            res.status(200).json({ response, msg: "Serviço recuperado com sucesso" });
+        } catch (error) {
+            console.log(error);
+        }
     }
+
 }
 
 export default serviceController;
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -11,6 +11,14 @@ router
     .route("/services")
     .post((req, res) => serviceController.create(req, res));
 
+//GET(Todos os serviços)
+router
+    .route("/services")
+    .get((req, res) => serviceController.getAll(req, res));
+
+//GET(serviço único)
+router.route("/services/:id")
+.get((req, res)=> serviceController.get(req, res));
 
 
 export default router;
