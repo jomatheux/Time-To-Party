32ee9e066fb59330f96cfd331147e81abf4be629
jomatheux <jomatheux@gmail.com>
2024-11-27 15:28:32 -0300

    Criando uma festa no banco de dados

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -1,7 +1,40 @@
 import Party from "../models/Party.js";
 
-const partyController = {
+const checkPartyBudget = (budget, services) =>{
+    const priceSum = services.reduce((sum, service) => sum + service.price, 0);
 
+    if(priceSum > budget) return false;
+
+    return true;
+
+}
+
+const partyController = {
+    // Função para criar party
+    create: async (req, res) =>{
+        try {
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services
+            }
+
+            if(party.services && !checkPartyBudget(party.budget, party.services)){
+                res.status(406).json({message: "O seu orçamento é insuficiente"});
+                return;
+            }
+
+            const response = await Party.create(party);
+
+            res.status(201).json({response ,message: 'Festa criada com sucesso' });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 export default partyController;
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -5,6 +5,8 @@ const router = express.Router();
 import partyController from '../controllers/partyController.js';
 
 //Funções
-
+//POST
+router.route('/party')
+    .post((req, res) => partyController.create(req, res));
 
 export default router;
