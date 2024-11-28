b85e4be189c19780cbfcaa23a31e058dfcd6d213
jomatheux <jomatheux@gmail.com>
2024-11-28 00:10:07 -0300

    Criando componente de navegação(NavBar)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 
+import Navbar from './components/Navbar.jsx'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Party Time</h1>
+      <Navbar />
       <Outlet />
     </>
   )
