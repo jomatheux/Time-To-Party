af0fd4fd80c60bb5889dd197c4df35f726a916e1
jomatheux <jomatheux@gmail.com>
2024-12-12 19:31:20 -0300

    Montando o objeto de festas com serviços

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -10,7 +10,15 @@ import './Form.css'
 
 const CreateParty = () => {
 
-  const[services, setServices] = useState([]);
+  const [services, setServices] = useState([]);
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [budget, setBudget] = useState(0);
+  const [image, setImage] = useState("");
+  const [partyServices, setPartyServices] = useState([]);
+
 
   //Carregando serviços
   useEffect(() => {
@@ -23,6 +31,37 @@ const CreateParty = () => {
     loadSevices();
   }, [])
 
+  //Adicionando/Removendo serviços
+  const handleServices = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+
+    const filteredService = services.filter((s) => s._id === value);
+
+    if (checked){
+      setPartyServices((services) => [...services, filteredService[0]]);
+    }else{
+      setPartyServices((services) => services.filter((s) => s._id !== value));
+    }
+
+    console.log(partyServices);
+  };
+
+  //Criando festa
+  const createParty = (e) => {
+    e.preventDefault();
+    const newParty = {
+      title,
+      author,
+      description,
+      budget,
+      image,
+      services: partyServices,
+    };
+
+    console.log(newParty);
+  };
+
 
 
 
@@ -30,45 +69,53 @@ const CreateParty = () => {
     <div className='form-page'>
       <h2>Crie sua próxima festa</h2>
       <p>Defina o seu orçamento e escolha seus serviços</p>
-      <form>
+      <form onSubmit={(e => createParty(e))}>
         <label>
           <span>Nome da festa:</span>
-          <input type="text" placeholder='Seja criativo...' required/>
+          <input type="text"
+            placeholder='Seja criativo...'
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title} />
         </label>
         <label>
           <span>Anfitrião:</span>
-          <input type="text" placeholder='Quem está dando a festa' required/>
+          <input type="text" placeholder='Quem está dando a festa' required onChange={(e) => setAuthor(e.target.value)}
+            value={author} />
         </label>
         <label>
           <span>Descrição:</span>
-          <textarea placeholder='Conte mais sobre a festa...' required></textarea>
+          <textarea placeholder='Conte mais sobre a festa...' required onChange={(e) => setDescription(e.target.value)}
+            value={description}></textarea>
         </label>
         <label>
           <span>Orçamento:</span>
-          <input type="number" placeholder='Quanto você pretende investir?' required/>
+          <input type="number" placeholder='Quanto você pretende investir?' required onChange={(e) => setBudget(e.target.value)}
+            value={budget} />
         </label>
         <label >
           <span>Imagem:</span>
-          <input type="text" placeholder='Insira a URL de uma imagem'/>
+          <input type="text" placeholder='Insira a URL de uma imagem' onChange={(e) => setImage(e.target.value)}
+            value={image} />
         </label>
         <div>
           <h2>Escolha os serviços</h2>
           <div className="services-container">
             {services.length === 0 && <p>Carregando...</p>}
-            {services.length >0 && services.map((service) =>(
+            {services.length > 0 && services.map((service) => (
               <div className='service' key={service._id}>
                 <img src={service.image} alt={service.name} />
                 <p className="service-name">{service.name}</p>
                 <p className="service-price">R${service.price}</p>
                 <div className="checkbox-container">
-                  <input type="checkbox" value={service._id}/>
+                  <input type="checkbox" value={service._id} onChange={(e) => handleServices(e)}/>
                   <p>Marque para solicitar</p>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <input type="submit" value="Criar Festa" className='btn'/>
+        <input type="submit" value="Criar Festa" className='btn' />
       </form>
     </div>
   )
